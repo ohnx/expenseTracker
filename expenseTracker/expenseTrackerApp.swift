4bc7d5c7566702000f1f9c4434c5af5d590aadfd
Mason <me@masonx.ca>
2023-06-10 10:29:59 -0400

    Initial Commit

---
+++ expenseTracker/expenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseTrackerApp.swift
+//  expenseTracker
+//
+//  Created by Mason on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct expenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
